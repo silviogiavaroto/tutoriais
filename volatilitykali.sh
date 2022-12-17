be9a525604933f70b54e19f9e9eb77a93df5baf0
Prof. Sílvio Giavaroto <119115141+silviogiavaroto@users.noreply.github.com>
2022-12-17 15:23:53 -0300

    Create volatilitykali.sh

---
+++ volatilitykali.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Install volatility 2 no Kali Linux 2020-4
+
+apt update
+apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
+
+apt install -y python2 python2.7-dev libpython2-dev
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+python2 -m pip install -U setuptools wheel
+
+python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+python2 -m pip install yara
+ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
